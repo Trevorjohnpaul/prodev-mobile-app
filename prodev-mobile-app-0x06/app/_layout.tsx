897a6c92d5442b5fdbfab75021fdb8ef67e656e5
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2026-01-18 17:56:12 +0300

    correction

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,11 +1,10 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-    </Stack>
+    <SafeAreaProvider>
+      <Slot />
+    </SafeAreaProvider>
   );
 }
